01fb4fa30f76de3552823f939db719ee3eeaa601
Aerys-cmd <recepobut72@gmail.com>
2024-08-24 14:15:23 +0300

    gitignore file added.

--- ITDeskServer/obj/Debug/net8.0/ITDeskServer.AssemblyInfo.cs
+++ ITDeskServer/obj/Debug/net8.0/ITDeskServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITDeskServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da971ea50470cd9cebc91bd3f23f21d2010fb7b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITDeskServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITDeskServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
