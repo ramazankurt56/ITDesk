7cf89b988d6201e2f4d326f08a8df2e70d45a048
ramazankurt56 <93058427+ramazankurt56@users.noreply.github.com>
2024-08-26 19:33:17 +0300

    kullanıcı giriş tamamlandı

--- ITDeskClient/src/app/models/login.model.ts
+++ ITDeskClient/src/app/models/login.model.ts
@@ -1,5 +1,5 @@
 export class LoginModel{
-    userNameOrEmail: string = "";
-    password: string = "";
+    userNameOrEmail: string = "test@test.com";
+    password: string = "Password12*";
     rememberMe: boolean =false;
 }
